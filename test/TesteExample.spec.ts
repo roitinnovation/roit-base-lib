a76c53e7c3f257cd959bcd4350175016322bb718
jeremiassilvaroit <jeremiassilvaroit@bitbucket.org>
2021-11-04 16:52:39 -0300

    migrate test to jest

--- test/TesteExample.spec.ts
+++ test/TesteExample.spec.ts
@@ -1,12 +1,10 @@
-import { expect } from 'chai';
-import 'mocha';
 describe('TesteExample tests', () => {
 
   it('is Hello', async () => {
 
     const helloHord = "Hello"
 
-    expect(helloHord).to.equal("Hello")
+    expect(helloHord).toBe("Hello")
 
   });
 
